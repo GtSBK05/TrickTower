7a65851c535de3d47b431dfdf52f7c0ede29e7cc
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2025-12-19 16:18:50 +0700

    Change main menu starting point to house

--- scenes/main/MainMenu.gd
+++ scenes/main/MainMenu.gd
@@ -1,8 +1,8 @@
 extends Control
 
 func _on_Start_pressed():
-	get_tree().change_scene_to_file("res://scenes/floors/Floor1_Awakening.tscn")
+	get_tree().change_scene_to_file("res://scenes/floors/house/house.tscn")
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/floors/Floor1_Awakening.tscn")
+	get_tree().change_scene_to_file("res://scenes/floors/house/house.tscn")
