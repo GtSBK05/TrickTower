2c19862b9e605d165ed9ae3f2e3d4f601e903b02
Fajar Rahmad Jaya <Fajar.Rahmad.Jaya.Profile@gmail.com>
2025-12-21 18:02:31 +0700

    Change default puzzle completion to true

--- autoload/GameState.gd
+++ autoload/GameState.gd
@@ -2,5 +2,7 @@ extends Node
 
 var scroll := 0
 var adik_dialogue_done = false
-var floor1_done = false
-var floor2_done = false
+
+# State if puzzle is complete 
+var floor1_done = true
+var floor2_done = true
